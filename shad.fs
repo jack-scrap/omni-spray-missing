3fa7520a5cf17b7b78fab8eb524b92b7c26e0f8c
Jack <jackhasakeyboard@gmail.com>
2022-05-10 21:11:48 -0600

    Added orange

--- shad.fs
+++ shad.fs
@@ -1,5 +1,10 @@
 #version 400
 
+vec3[2] col = vec3[2](
+	vec3(38, 38, 38),
+	vec3(255, 142, 65)
+);
+
 void main() {
-	gl_FragColor = vec4(vec3(1.0), 1.0);
+	gl_FragColor = vec4(col[int(true)] / 255.0, 1.0);
 }
