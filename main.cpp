92e739d1e72515e27d242e02cdc1faf8fa3fdf3a
Jack <jackhasakeyboard@gmail.com>
2022-05-10 21:21:46 -0600

    Loaded data from file, incoming positions now 3 dimensional again

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 
 #include "disp.h"
 #include "prog.h"
@@ -11,6 +16,65 @@ static constexpr glm::vec3 col[2] = {
 	glm::vec3(38, 38, 38),
 	glm::vec3(255, 142, 65)
 };
+
+std::vector<std::string> split(std::string buff, char delim) {
+	std::vector<std::string> tok;
+
+	std::stringstream s(buff);
+	std::string seg;
+	while (std::getline(s, seg, delim)) {
+		tok.push_back(seg);
+	}
+
+	return tok;
+}
+
+std::vector<std::string> rd(std::string fName) {
+	std::ifstream in;
+	in.open(fName);
+
+	std::vector<std::string> cont;
+
+	for (std::string l; std::getline(in, l);) {
+		cont.push_back(l);
+	}
+
+	in.close();
+
+	return cont;
+}
+
+std::vector<GLfloat> rdAttr(std::string fName, unsigned int attr) {
+	std::vector<GLfloat> _;
+
+	std::vector<std::string> buff = rd(fName + ".obj");
+
+	const std::string id[3] = {
+		"v",
+		"vt",
+		"vn"
+	};
+	const unsigned int sz[3] = {
+		3,
+		2,
+		3
+	};
+
+	for (int l = 0; l < buff.size(); l++) {
+		std::vector<std::string> tok = split(buff[l], ' ');
+
+		if (tok[0] == id[attr]) {
+			for (int i = 1; i < 1 + sz[attr]; i++) {
+				std::stringstream out;
+				out << std::fixed << std::setprecision(4) << std::stof(tok[i]);
+
+				_.push_back(std::stof(out.str()));
+			}
+		}
+	}
+
+	return _;
+}
 
 int main() {
 	Disp disp("asdf", 1000, 1000);
@@ -25,12 +89,8 @@ int main() {
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-	GLfloat vtc[] = {
-		-1.0, -1.0,
-		1.0, -1.0,
-		-1.0, 1.0
-	};
-	glBufferData(GL_ARRAY_BUFFER, sizeof vtc, vtc, GL_STATIC_DRAW);
+	std::vector<GLfloat> vtc = rdAttr("asdf", 0);
+	glBufferData(GL_ARRAY_BUFFER, vtc.size() * sizeof (GLfloat), &vtc[0], GL_STATIC_DRAW);
 
 	// matrix
 	glm::mat4 model = glm::mat4(1.0);
@@ -40,7 +100,7 @@ int main() {
 
 	/// attribute
 	GLint attrPos = glGetAttribLocation(prog._id, "pos");
-	glVertexAttribPointer(attrPos, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
+	glVertexAttribPointer(attrPos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
 	glEnableVertexAttribArray(attrPos);
 
 	/// uniform
@@ -61,7 +121,7 @@ int main() {
 
 		disp.clear(col[false].r / 255.0, col[false].g / 255.0, col[false].b / 255.0, 1);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, vtc.size() / 3);
 
 		disp.update();
 	}
--- shad.vs
+++ shad.vs
@@ -1,9 +1,9 @@
 #version 400
 
-in vec2 pos;
+in vec3 pos;
 
 uniform mat4 model;
 
 void main() {
-	gl_Position = model * vec4(vec3(pos, 0.0), 1.0);
+	gl_Position = model * vec4(pos, 1.0);
 }
