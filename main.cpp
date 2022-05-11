c83c2b7ed63eae797dfddb11b19f56a0c35a82f3
Jack <jackhasakeyboard@gmail.com>
2022-05-10 21:36:29 -0600

    Floated model to top-left

--- main.cpp
+++ main.cpp
@@ -123,6 +123,7 @@ int main() {
 	const GLfloat scaleFac = 0.1;
 
 	glm::mat4 model = glm::mat4(1.0);
+	model = glm::translate(model, glm::vec3(-1.0, 1.0, 0.0));
 	model = glm::scale(model, glm::vec3(scaleFac, scaleFac, scaleFac));
 
 	// shader
