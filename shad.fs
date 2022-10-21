c0903f22c118769883f39d34753fc6d0dfdf4efd
Jack <jackhasakeyboard@gmail.com>
2022-10-21 04:46:55 -0600

    Drew modulating checkerboard shader

--- shad.fs
+++ shad.fs
@@ -1,10 +1,14 @@
 #version 400
 
+in vec3 _pos;
+
+const float stride = 1.0;
+
 vec3[2] col = vec3[2](
 	vec3(38, 38, 38),
 	vec3(255, 142, 65)
 );
 
 void main() {
-	gl_FragColor = vec4(col[int(false)] / 255.0, 1.0);
+	gl_FragColor = vec4(col[int(mod(_pos.x, stride * 2) > stride ^^ mod(_pos.y, stride * 2) > stride)] / 225.0, 1.0);
 }
--- shad.vs
+++ shad.vs
@@ -2,6 +2,10 @@
 
 in vec3 pos;
 
+out vec3 _pos;
+
 void main() {
 	gl_Position = vec4(pos, 1.0);
+
+	_pos = pos * vec3(1, -1, 1);
 }
