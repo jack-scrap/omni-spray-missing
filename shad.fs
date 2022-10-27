2009e51d193bf4d22017f70aa007b368573275dd
Jack <jackhasakeyboard@gmail.com>
2022-10-27 05:15:52 -0600

    Inverted boolean expression in shader, now properly begins with an inert square where inverted screen-space texture coordinates place a negative value at the bottom left

--- shad.fs
+++ shad.fs
@@ -12,5 +12,5 @@ vec3[2] col = vec3[2](
 );
 
 void main() {
-	gl_FragColor = vec4(col[int(mod(_pos.x, stride * 2) > stride ^^ mod(_pos.y, stride * 2) > stride)] / 225.0, 1.0);
+	gl_FragColor = vec4(col[int(!(mod(_pos.x, stride * 2) > stride ^^ mod(_pos.y, stride * 2) > stride))] / 225.0, 1.0);
 }
